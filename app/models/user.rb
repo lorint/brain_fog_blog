ebbde4cf0c5412ccd6ade3298a25032de23ce298
admin <admin@admins-MacBook-Pro-8.local>
2015-02-25 12:12:08 -0800

    Some custom validation added to have either handle or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,27 @@ class User < ActiveRecord::Base
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	validates_format_of :email, with: /\A[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z/
+	
+	# TWO alternatives to validate presence of both handle and email:
+
+    # validates :email, presence: true
+    # validates :handle, presence: true
+
+#   validates_presence_of :email, :handle
+
+	# Now we want to have either handle or email, or both
+	# But it's can't have both email and handle blank
+
+	validate :either_handle_or_email
+
+	def either_handle_or_email
+		if (self.handle && self.handle.length > 0) ||
+			(self.email && self.email.length > 0)
+			return
+		else
+			self.errors.add(:email, "Must specify either handle or email")
+			self.errors.add(:handle, "Must specify either handle or email")
+		end
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217183952) do
+ActiveRecord::Schema.define(version: 20150225194924) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment_text"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150217183952) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "handle"
   end
 
 end
